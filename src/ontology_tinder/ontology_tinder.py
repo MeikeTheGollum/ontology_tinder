31666c6f5625542653fccaa93e9d66842fd7b717
wienholt <wienholt@uni-bremen.de>
2024-09-23 11:50:42 +0200

    removed print statements

--- src/ontology_tinder/ontology_tinder.py
+++ src/ontology_tinder/ontology_tinder.py
@@ -28,9 +28,7 @@ class OntologyTinder:
 
         for concept in self.ontology.classes():
             data.append(concept.name.split("_"))
-        print(data)
         model = Word2Vec(data, min_count=1, vector_size=100, window=5)
-        print(model)
         word_vectors = model.wv
         # tmp = {concept: model.wv[concept.name.split("_")[0]] for concept in self.ontology.classes
         #         if concept.name.split("_")[0] in model}
@@ -44,12 +42,10 @@ class OntologyTinder:
         :return: The most similar concepts
         """
         # convert the name to a token
-        #model = Word2Vec[name]
         vector = self.concept_embeddings[name]
 
         # compare with tokens from self.concept_embeddings
         result = self.concept_embeddings.most_similar(positive=name)
-        print(result)
         return result
 
     def most_similar_concept_of_names(self, names: typing_extensions.List[str]) -> typing_extensions.List[owlready2.Thing]:
@@ -67,5 +63,4 @@ class OntologyTinder:
         # compare with tokens from self.concept_embeddings
         result = [self.concept_embeddings.most_similar(positive=name) for name in names ]
         # return the most similar concepts
-        print(result)
         return result
