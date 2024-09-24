0db8cecc8affd93865b13630d94560a551fddcf6
Tom Schierenbeck <tom_sch@uni-bremen.de>
2024-09-24 11:33:23 +0200

    We need to load a pretrained thingy first

--- src/ontology_tinder/ontology_tinder.py
+++ src/ontology_tinder/ontology_tinder.py
@@ -1,92 +1,104 @@
 from functools import cached_property
 
 import owlready2
+from gensim.models import Word2Vec, KeyedVectors
+import gensim.downloader
 from owlready2 import Ontology
-from gensim.models import Word2Vec
-import nltk
-#from typing_extensions import List, Any, Dict
-import typing_extensions
+from typing_extensions import List, Dict, Tuple, Optional
+
+
+def prune_name(name: str):
+    return name.split("_")[0]
 
 class OntologyTinder:
-
     ontology: Ontology
+
+    _concept_embeddings: Optional[KeyedVectors] = None
 
     def __init__(self, ontology: Ontology):
         self.ontology = ontology
         self.ontology.load()
 
+    def matching_concepts_by_name(self, name: str) -> List[owlready2.Thing]:
+        return [concept for concept in self.ontology.classes() if concept.name.split("_")[0] == name]
+
+    def matching_concept_by_name(self, name: str) -> owlready2.Thing:
+        result = self.matching_concepts_by_name(name)
+        if len(result) == 0:
+            raise ValueError(f"No concept found for name {name}")
+        elif len(result) > 1:
+            raise ValueError(f"Multiple concepts found for name {name}. Concepts found: {result}")
+        return self.matching_concepts_by_name(name)[0]
+
     @cached_property
-    def concept_embeddings(self) -> typing_extensions.Dict[owlready2.Thing, typing_extensions.Any]:
+    def concept_names(self) -> List[str]:
+        return [concept.name for concept in self.ontology.classes()]
+
+    @property
+    def concept_embeddings(self) -> KeyedVectors:
         """
         Calculate the embeddings for all concepts in the ontology.
 
-        TODO Check that this generates the desired dictionary
-
         :return: The embeddings for all concepts
         """
-        data = []
+        if not self._concept_embeddings:
+            data = [[name] for name in self.concept_names]
+            model = Word2Vec(data, min_count=1, vector_size=100, window=5)
+            self._concept_embeddings = model.wv
+        return self._concept_embeddings
 
-        for concept in self.ontology.classes():
-            data.append(concept.name.split("_"))
+    @concept_embeddings.setter
+    def concept_embeddings(self, value: KeyedVectors):
+        self._concept_embeddings = value
+
+    def expand_vocabulary(self, new_words: List[str]):
+        """
+        TODO
+        """
+        data = self.concept_names + new_words
+        data = [[name] for name in data]
         model = Word2Vec(data, min_count=1, vector_size=100, window=5)
-        word_vectors = model.wv
-        # tmp = {concept: model.wv[concept.name.split("_")[0]] for concept in self.ontology.classes
-        #         if concept.name.split("_")[0] in model}
-        return word_vectors
+        return model.wv
 
-    def most_similar_concept_of_name(self, name: str) -> typing_extensions.List[owlready2.Thing]:
-        """
-        Find the most similar concepts for a given string in the ontology.
-        This uses a word embedding model for all concepts and the name to calculate the most similar concept.
-        :param name: The name
-        :return: The most similar concepts
-        """
-        # convert the name to a token
-        vector = self.concept_embeddings[name]
-
-        # compare with tokens from self.concept_embeddings
-        result = self.concept_embeddings.most_similar(positive=name)
-        return result
-
-    def most_similar_concept_of_names(self, names: typing_extensions.List[str]) -> typing_extensions.List[owlready2.Thing]:
+    def most_similar_concept_of_names(self, names: List[str], top_n: int=5) -> Dict[str, Tuple[owlready2.Thing, float]]:
         """
         Find the most similar concepts for a list of names in the ontology.
         This uses a word embedding model for all concepts and the name to calculate the most similar concept.
 
         :param names: The names
+        :param top_n: The number of most similar concepts to return
         :return: The most similar concepts
         """
+        new_names = [name for name in names if name not in self.concept_embeddings.key_to_index.keys()]
 
-        # convert the names to tokens
-        model1 = Word2Vec(names, min_count=1, vector_size=100, window=5)
+        if len(new_names) > 0:
+            word_vectors = self.expand_vocabulary(new_names)
+        else:
+            word_vectors = self.concept_embeddings
 
-        # compare with tokens from self.concept_embeddings
-        result = [self.concept_embeddings.most_similar(positive=name) for name in names ]
-        # return the most similar concepts
+        result = dict()
+        for name in names:
+            current_result = [(concept, 1.) for concept in self.matching_concepts_by_name(name)]
+            match_by_embedding = word_vectors.most_similar(positive=[name], topn=top_n)
+            print(match_by_embedding)
+            current_result.extend([(self.matching_concept_by_name(name), score) for name, score in
+                                   match_by_embedding[:top_n - len(current_result)]])
+            result[name] = current_result
         return result
 
-    def closest_concept_of_name(self, name: str) -> typing_extensions.List[owlready2.Thing]:
+    def closest_concept_of_name(self, name: str) -> owlready2.Thing:
         """
-        Instead of searching for a list of most similar matches, this returns the first entry of the list of most similar
-        concepts of a given name.
+        Get the closest concept for a given name.
 
         :param name: The name
         :return: The most likely concept
         """
-        return self.most_similar_concept_of_name(name)[0]
+        result = self.most_similar_concept_of_names([name])
+        return result[name][0][0]
 
-    def closest_concept_of_names(self, names: typing_extensions.List[str]) -> typing_extensions.List[owlready2.Thing]:
-        """
-        Instead of searching for a list of most similar matches, this returns the first entry of the list of most similar
-        concepts of a given list of names.
 
-        :param name: The names
-        :return: The most likely concepts
-        """
-        return [concepts[0] for concepts in self.most_similar_concept_of_names(names)]
-
-    #TODO: Implement a function, that adds a new key into the list of vectors, so that we can test with new words outside
+    # TODO: Implement a function, that adds a new key into the list of vectors, so that we can test with new words outside
     # of the scope of the generated vector list from the ontology
 
-    #TODO: Ask Tom, if I should include the URIs of the found concepts (e.g.  "http://test.owl#TestEntry" or if he needs
+    # TODO: Ask Tom, if I should include the URIs of the found concepts (e.g.  "http://test.owl#TestEntry" or if he needs
     # any information besides that
--- test/word_embeddings_tests.py
+++ test/word_embeddings_tests.py
@@ -2,15 +2,14 @@ import unittest
 
 from owlready2 import get_ontology
 
-from src.ontology_tinder.ontology_tinder import OntologyTinder
-#from src.ontology_tinder import ontology_tinder.OntologyTinder
-from src.ontology_tinder import word_embeddings
+from ontology_tinder.ontology_tinder import OntologyTinder
 
 
-class WordEmbeddingsTestCase(unittest.TestCase):
-
-    def test_getSimilarity(self):
-        self.assertEqual(word_embeddings.getSimilarity('alarmclock_1', 'alarmclock_1'), 1.0)
+#
+# class WordEmbeddingsTestCase(unittest.TestCase):
+#
+#     def test_getSimilarity(self):
+#         self.assertEqual(word_embeddings.getSimilarity('alarmclock_1', 'alarmclock_1'), 1.0)
 
 
 class MinimalOntologyTinderTestCase(unittest.TestCase):
@@ -28,37 +27,20 @@ class MinimalOntologyTinderTestCase(unittest.TestCase):
         concept_embeddings = self.ot.concept_embeddings
         self.assertEqual(len(concept_embeddings), 3)
 
-    def test_ontology_tinder_single_name(self):
-        most_similar_concepts = self.ot.most_similar_concept_of_name("alarmclock")
-        # because we test against an ontology with three entries, only 2 of them can be
-        # most similar to the one name
-        self.assertEqual(len(most_similar_concepts), 2)
-        c1, c2 = most_similar_concepts
+    def test_ontology_tinder_single_names(self):
+        most_similar_concepts = self.ot.most_similar_concept_of_names(["alarmclock"], 3)
+        self.assertEqual(len(most_similar_concepts), 1)
 
-        # useful comparisons here
-        self.assertEqual(c1, ('dishwasher', -0.02367166429758072))
-        self.assertEqual(c2, ('wall', -0.052346739917993546))
-    def test_ontology_tinder_list_names(self):
-        most_similar_concepts = self.ot.most_similar_concept_of_names(["alarmclock", "wall"])
-        self.assertEqual(len(most_similar_concepts), 2)
-        c1, c2 = most_similar_concepts
-        self.assertEqual(c1[0], ('dishwasher', -0.02367166429758072) )
-        self.assertEqual(c1[1], ('wall', -0.052346739917993546) )
-        self.assertEqual(c2[0], ('dishwasher', -0.010839177295565605))
-        self.assertEqual(c2[1], ('alarmclock', -0.05234673619270325))
+        alarm_clock_similarity = most_similar_concepts["alarmclock"]
 
-    def test_ontology_tinder_single_name_closest(self):
-        closest_match = self.ot.closest_concept_of_name("wall")
-        self.assertEqual(len([closest_match]), 1)
-        c1 = closest_match
-        self.assertEqual(c1, ('dishwasher', -0.010839177295565605))
+        self.assertEqual(len(alarm_clock_similarity), 3)
+        concepts = [c for c,_ in alarm_clock_similarity]
+        self.assertEqual(set(concepts), set(self.ot.ontology.classes()))
 
-    def test_ontology_tinder_list_names_closest(self):
-        closest_matches = self.ot.closest_concept_of_names(["alarmclock", "wall"])
-        self.assertEqual(len(closest_matches), 2)
-        c1, c2 = closest_matches
-        self.assertEqual(c1, ('dishwasher', -0.02367166429758072))
-        self.assertEqual(c2, ('dishwasher', -0.010839177295565605))
+    def test_get_closest_concept_of_name(self):
+        similar_concept = self.ot.closest_concept_of_name("clock")
+        self.assertEqual(similar_concept.name, "alarmclock")
+
 
 class OntologyTinderTestCase(unittest.TestCase):
 
